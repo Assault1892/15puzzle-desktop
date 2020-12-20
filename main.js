c8562b386c837ba1eaa0bdc86c0bbfa2ae7c4df5
Assault.8448 <34514603+Assault-8448@users.noreply.github.com>
2020-12-20 13:09:44 +0900

    hmm

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+	let win = new BrowserWindow({
+		width: 450,
+		height: 600,
+		resizable: false,
+		maximizable: false,
+		center: true,
+		autoHideMenuBar: true,
+		hasShadow: false,
+		icon: (__dirname + '/icon.png'),
+		backgroundColor: "#222",
+		title: "15 Puzzle"
+	});
+	
+	win.loadURL('https://15-puzzle.app');
+};
+
+app.on('ready', createWindow);
