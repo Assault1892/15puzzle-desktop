c8811e6b0824d5d71fc2e8df6e26a6cd8f3f0371
Assault.8448 <34514603+Assault-8448@users.noreply.github.com>
2020-12-20 14:20:11 +0900

    Electron-Builder をインストールするために色々した
    npm i -D electron, electron-builder　を実行した。恐らく支障はないとは思うけど・・・一応コミット。

---
+++ build.js
@@ -0,0 +1,16 @@
+const builder = require('electron-builder');
+
+builder.build({
+    config: {
+        'appId': '15puzzle.desktop',
+        'win':{
+            'target': {
+                'target': 'zip',
+                'arch': [
+                    'x64',
+                    'ia32',
+                ]
+            }
+        }
+    }
+});
